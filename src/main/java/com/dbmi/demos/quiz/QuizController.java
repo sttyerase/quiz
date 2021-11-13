a5ce3a0bb82824d0f45999c7fc7e3874ad68bd32
D. B. Moore <sttyerase@gmail.com>
2021-11-13 10:11:29 -0500

    Resolve communication between controller and templates.

--- src/main/java/com/dbmi/demos/quiz/QuizController.java
+++ src/main/java/com/dbmi/demos/quiz/QuizController.java
@@ -1,5 +1,6 @@
 package com.dbmi.demos.quiz;
 
+import com.dbmi.demos.quiz.model.Question;
 import com.dbmi.demos.quiz.model.QuestionList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,15 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.Vector;
+
 import org.springframework.ui.Model;
 
 import com.dbmi.demos.quiz.model.Quiz;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -26,34 +27,90 @@ public class QuizController {
     private Vector<Quiz> myQuizes;
 
     // MAPPINGS
-
     @RequestMapping("/")
-    public String home(Model aModel, HttpServletRequest request, HttpServletResponse response) {
-        return this.welcome(aModel, request, response);
+    public String home(Model aModel, HttpServletRequest request) {
+        myLogger.info("Requested root page.");
+        return this.welcome(aModel, request);
     } // HOME
 
     @RequestMapping("/welcome")
-    public String welcome(Model aModel, HttpServletRequest request, HttpServletResponse response) {
+    public String welcome(Model aModel, HttpServletRequest request) {
+        myLogger.info("Requested welcome page.");
+        request.getSession().invalidate();
         HttpSession mySession = request.getSession(true);
-        aModel.addAttribute("myQuizes",myQuizes);
-        aModel.addAttribute("today",new Date().toString());
+        mySession.setAttribute("questionnumber", 0);
+        aModel.addAttribute("myQuizes", myQuizes);
+        aModel.addAttribute("today", new Date().toString());
         return "welcome";
     } // WELCOME(MODEL)
 
-    @PostMapping("/displayquestion")
-    public String quizquestion(Model aModel, HttpServletRequest request, HttpServletResponse response) {
+    @PostMapping("/initiatequiz")
+    public String initiatequiz(Model aModel, HttpServletRequest request) {
         // SESSION MANAGEMENT
-        HttpSession mySession = request.getSession();
-        Enumeration<String> en = mySession.getAttributeNames(); // TODO: REMOVE AFTER DEBUGGING
-        // GET PARAMETERS
+        myLogger.info("INITIATEQUIZ: page requested.");
+        HttpSession mySession = request.getSession(false);
+        if (mySession == null) {
+            myLogger.info("INITIATEQUIZ: no session passed with request. Exiting now.");
+            return this.error(aModel,request,"INITIATEQUIZ: no session passed with request. Exiting now.");
+        } // IF(MYSESSION)
         int quiznum = Integer.parseInt(request.getParameter("welcomeselect"));
         Quiz theQuiz = myQuizes.elementAt(quiznum);
-        String quizName = theQuiz.getQuizName();
+        theQuiz.getQuestionList().initQuestionList();
+        theQuiz.getScoreKeeper().init();
+        mySession.setAttribute("thequiz", theQuiz);
+        return this.displayquestion(aModel,request);
+    } // INITIATEQUIZ(MODEL,HTTPSERVLETREQUEST,HTTPSERVLETRESPONSE)
+
+    @RequestMapping("/displayquestion")
+    public String displayquestion(Model aModel, HttpServletRequest request) {
+        myLogger.info("DISPLAYQUESTION: requested.");
+        Quiz theQuiz = (Quiz) request.getSession().getAttribute("thequiz");
+        // IF THE QUIZ IS DONE, RETURN THE RESULTS
+        if (theQuiz.getQuestionList().notDone()) {
+            /* CONTINUE ON */
+        } else {
+            return this.quizresults(aModel, request);
+        } // IF(QL.NOTDONE)
         QuestionList theQuestionList = theQuiz.getQuestionList();
+        int currentQuestionNumber = theQuestionList.getCurrentQuestionNumber();
+        theQuestionList.setCurrentQuestionNumber(currentQuestionNumber);
+        Question currentQuestion = theQuestionList.getTheQuestions().elementAt(currentQuestionNumber);
+        int numberOfQuestions = theQuestionList.getNumberOfQuestions();
         // ADD COMPONENTS TO THE MODEL
-        aModel.addAttribute("thequiz",theQuiz);
-        aModel.addAttribute("today",new Date().toString());
+        aModel.addAttribute("quizname", theQuiz.getQuizName());
+        aModel.addAttribute("numberofquestions", numberOfQuestions);
+        aModel.addAttribute("currentquestion", currentQuestion);
+        aModel.addAttribute("userquestionnumber", currentQuestionNumber + 1);  // THE USER QUESTION NUMBER IS ARRAY ADDRESS + 1
+        aModel.addAttribute("today", new Date().toString());
         return "quizquestion";
-    } // QUIZQUESTION(MODEL)
+    } // QUIZQUESTION(MODEL,HTTPSERVLETREQUEST,HTTPSERVLETRESPONSE)
+
+    @RequestMapping("/displayanswer")
+    public String theanswer(Model aModel, HttpServletRequest request) {
+        myLogger.info("DISPLAYANSWER: requested.");
+        Quiz theQuiz = (Quiz) request.getSession().getAttribute("thequiz");
+        aModel.addAttribute("thequiz",theQuiz);
+        aModel.addAttribute("currentquestionnumber",theQuiz.getQuestionList().getCurrentQuestionNumber());
+        aModel.addAttribute("today", new Date().toString());
+        theQuiz.getQuestionList().setCurrentQuestionNumber(theQuiz.getQuestionList().getCurrentQuestionNumber() + 1);
+        return "theanswer";
+    } // THEANSWER(MODEL,HTTPSERVLETREQUEST,HTTPSERVLETRESPONSE)
+
+    @RequestMapping("/displayresults")
+    public String quizresults(Model aModel, HttpServletRequest request) {
+        myLogger.info("DISPLAYRESULTS: requested.");
+        Quiz theQuiz = (Quiz) request.getSession().getAttribute("thequiz");
+        aModel.addAttribute("today", new Date().toString());
+        return "quizresults";
+    } // QUIZRESULTS(MODEL,HTTPSERVLETREQUEST,HTTPSERVLETRESPONSE)
+
+    @RequestMapping("/newerror")
+    public String error(Model aModel, HttpServletRequest request, String errorMessage) {
+        myLogger.info("NEWERROR: requested.");
+        Quiz theQuiz = (Quiz) request.getSession().getAttribute("thequiz");
+        aModel.addAttribute("err", errorMessage);
+        aModel.addAttribute("today", new Date().toString());
+        return "error";
+    } // ERROR(MODEL,HTTPSERVLETREQUEST,HTTPSERVLETRESPONSE)
 
 } // CLASS
